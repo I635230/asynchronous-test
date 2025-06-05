1700055ef357c00caeb1b6fada423dc63d734b7e
I635230 <yugo.susuki@gmail.com>
2025-06-06 01:01:38 +0900

    タスクの並列実行

--- app/main.py
+++ app/main.py
@@ -6,26 +6,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# FastAPI
 async def start_fastapi():
     config = uvicorn.Config(app=app, host="0.0.0.0", port=8000)
     server = uvicorn.Server(config)
     await server.serve()
 
-
-# dummy log
-async def dummy_log(): 
+async def dummy_log():
     print("bot起動ログ想定")
+    while True:
+        await asyncio.sleep(10)
 
-# main
 async def main():
     load_dotenv()
     
     try:
-        await asyncio.gather(
-            start_fastapi(),
-            dummy_log()
-        )
+        # 個別にタスクを作成して並列実行させる
+        tasks = [
+            asyncio.create_task(start_fastapi()),
+            asyncio.create_task(dummy_log()),
+        ]
+        await asyncio.gather(*tasks)
     except Exception as e:
         print(f"起動中にエラーが発生しました: {e}")
 
